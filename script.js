165bb62d5fefe6c926b82585669edff27b55bb97
Anmol Sangha <anmolsangha02@gmail.com>
2024-07-17 03:18:44 +0530

    created function to displaydata in product cards

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ function filterProducts(categoryName) {
     });
 }
 
+function displayProducts(products) {
+    const productContainer = document.getElementById('product-container');
+    productContainer.innerHTML = '';
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card';
+
+        productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.title}" class="product-image">
+            <div class="product-info">
+                <div class="product-title">${product.title}</div>
+                <div class="product-vendor">${product.vendor}</div>
+            </div>
+            <div class="price-info">
+                <div class="product-price">Rs ${product.price}</div>
+                <div class="product-compare">${product.compare_at_price}.00</div>
+                <div class="discount">${discount}% Off</div>
+            </div>
+            <button class="add-to-cart">Add to Cart</button>
+        `;
+        productContainer.appendChild(productCard);
+    });
+}
+
 filterProducts('Men');

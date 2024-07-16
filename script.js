daf87a8d50bf55e5f744f8b61e544527ff0200a4
Anmol Sangha <anmolsangha02@gmail.com>
2024-07-17 03:13:57 +0530

    Fetch product data from API

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// core functionality goes here
+async function fetchProducts() {
+    const response = await fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json');
+    const data = await response.json();
+    return data.categories;
+}
+
+function filterProducts(categoryName) {
+    fetchProducts().then(categories => {
+        const category = categories.find(cat => cat.category_name === categoryName);
+        displayProducts(category.category_products);
+    });
+}
+
+filterProducts('Men');
